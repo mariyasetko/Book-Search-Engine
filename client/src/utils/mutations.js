ef57e8272f825aa86b4034a63e50b17e0993b46c
Mariya Setko <mariya.setko@gmail.com>
2022-07-28 20:44:00 -0400

    SavedBooks and SearchBooks

--- client/src/utils/mutations.js
+++ client/src/utils/mutations.js
@@ -1,8 +1,5 @@
 import { gql } from "@apollo/client";
 
-
-
-
 export const LOGIN_USER = gql`
     mutation login($email: String!, $password: String!){
         login(email: $email, password: $password){
@@ -26,7 +23,6 @@ export const ADD_USER = gql`
         }
     }
 `;
-
 
 export const SAVE_BOOK = gql`
     mutation: saveBook($book: BookInput!){
