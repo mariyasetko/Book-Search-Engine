763f29481e5b750821c1e06b6a29e5eeebd7ee4d
Mariya Setko <mariya.setko@gmail.com>
2022-07-29 20:19:55 -0400

    fix

--- server/config/connection.js
+++ server/config/connection.js
@@ -1,7 +1,7 @@
 const mongoose = require('mongoose');
 
 mongoose.connect(
-  process.env.MONGODB_URI || 'mongodb://localhost/bookDB',
+  process.env.MONGODB_URI || 'mongodb://localhost:27017/bookDB',
   {
     useNewUrlParser: true,
     useUnifiedTopology: true,
